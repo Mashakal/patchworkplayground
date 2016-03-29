ae62bb695bfebbc468dcbc9e11251d2528bc8852
Alex Neuenkirk <alex.neuenkirk@gmail.com>
2016-03-29 09:33:46 -0400

    Implemented a basic entry/start scene.

---
+++ Assets/Resources/Scripts/StartButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+
+    public void StartAdventure()
+    {
+        SceneManager.LoadScene("betaLevel1");
+    }
+}
