7db4927b928f62280ca99ed98181f52b32cdb670
Alex Neuenkirk <alex.neuenkirk@gmail.com>
2016-05-05 12:40:03 -0400

    Implemented resetting player objects.

---
+++ Assets/Resources/Scripts/OnSceneLoad.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnSceneLoad : MonoBehaviour {
+
+    // References.
+    Vector3 startingPosition;
+    GameObject player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        startingPosition = GetComponentInChildren<Transform>().position;
+        player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<Transform>().position = startingPosition;
+        player.GetComponent<JumpStamper>().Reset();
+        player.GetComponent<TrailingStamps>().Reset();
+	}
+}
