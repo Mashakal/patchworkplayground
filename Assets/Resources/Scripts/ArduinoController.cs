3fcd5d79c3b8142d8a5764493089769185ce66ca
Anne <anne@play-crafts.com>
2016-07-13 11:07:29 -0400

    added Uniduino and started working on getting the arduino hooked up to the game

---
+++ Assets/Resources/Scripts/ArduinoController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Uniduino;
+
+public class ArduinoController : MonoBehaviour {
+
+	public Arduino arduino;
+
+	// Use this for initialization
+	void Start () {
+		arduino = Arduino.global;
+		arduino.Setup (ConfigurePins);
+	}
+
+	void ConfigurePins () {
+		arduino.pinMode (13, PinMode.OUTPUT);
+		StartCoroutine (BlinkLoop ());
+	}
+
+	IEnumerator BlinkLoop() {
+		while (true) {
+			arduino.digitalWrite (13, Arduino.HIGH);
+			yield return new WaitForSeconds (1);
+			arduino.digitalWrite (13, Arduino.LOW);
+			yield return new WaitForSeconds (1);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
